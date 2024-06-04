961edd2aade847201bf21e58f798e03db039813b
Lucas Wolff <lucaswolff2014@hotmail.com>
2024-06-04 19:11:01 -0300

    Changes 24

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <html lang="en">
       <Head>
-        <title>Doomsday Logs</title>
+        <title>Doomsday </title>
         <link rel="icon" href="/doomsdaylogs-site/images/icone.ico" />
       </Head>
       <body>
